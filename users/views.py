418225a14c3ddb42784465d5e02892c880b8e2c4
Willem <willemarf@gmail.com>
2015-07-15 16:30:21 -0300

    api auth

--- users/views.py
+++ users/views.py
@@ -4,16 +4,21 @@ from restless.views import Endpoint
 from models import User, SocialNetwork, Friend
 from restless.models import serialize
 from django.db.models import Q
+from restless.auth import BasicHttpAuthMixin, login_required
 
 # class SecretGreeting(Endpoint, BasicHttpAuthMixin):
 #     @login_required
 #     def get(self, request):
 #         return {'message': 'Hello, %s!' % request.user}
 
+__all__ = ['UsernamePasswordAuthMixin', 'BasicHttpAuthMixin',
+    'AuthenticateEndpoint', 'login_required']
+    
 class UserList(ListEndpoint):
     model = User
 
 class UserDetail(Endpoint):
+    @login_required
     def get(self, request, pk):
         user = User.objects.get(pk=pk)
 
